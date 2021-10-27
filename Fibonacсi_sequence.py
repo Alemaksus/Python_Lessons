2f9c27d8b947c7e474704923fba4be2dfe55bb6f
Aleksandr Maksakov <58271708+Alemaksus@users.noreply.github.com>
2021-10-27 12:52:36 +0300

    Iterator for generating the Fibonacci sequence with stop at number 650

--- Fibonacсi_sequence.py
+++ Fibonacсi_sequence.py
@@ -1,19 +1,27 @@
 from itertools import islice
 
-class Fib:
-    def __init__(self):
+class FibFinit:
+    def __init__(self, stop=650): # Stop for number 650
         self.prev = 0
         self.curr = 1
+        self.stop = stop
 
     def __iter__(self):
         return self
 
     def __next__(self):
+        if self.curr > self.stop:
+            raise StopIteration
         value = self.curr
         self.curr += self.prev
         self.prev = value
         return value
 
 
-fib = Fib()
-print(list(islice(fib, 1, 15)))
+fib = FibFinit()
+print(list(islice(fib, 1, 30)))
+
+fib = FibFinit()
+print(next(fib))
+print(next(fib))
+print(next(fib))
